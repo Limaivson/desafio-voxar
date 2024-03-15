7ebc9c0db5654c337ceb9da0bf4f6bccb9b8761f
Limaivson <ils3@cin.ufpe.br>
2024-03-15 15:25:10 -0300

    refactor

--- server/server/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    # Route to the prediction view
-    path('receive_path/', views.receive_path, name='receive_path'),
-]
